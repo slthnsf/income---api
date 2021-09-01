ba3b10d0bbab2cf954dc5f2a8e0fa6b37f200c8d
slthnsf <ssafinatunnajah@gmail.com>
2021-09-01 09:18:41 +0700

    Add readme, update expanses controller

---
+++ README.md
@@ -0,0 +1,105 @@
+# Income API Documentation
+
+## Authentication
+
+### Login
+Request:
+- Method: POST
+- Endpoint: `/user/login`
+- Body:
+```json
+{
+    "email": "admin@mail.com",
+    "password": "admin123"
+}
+```
+Response:
+```json
+{
+    "iduser": 1,
+    "username": "admin",
+    "fullname": "Admin",
+    "email": "admin@mail.com",
+    "password": "admin123",
+    "idstatus": 1,
+    "idrole": 1,
+    "otp": "axYbc",
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHVzZXIiOjEsInVzZXJuYW1lIjoiYWRtaW4iLCJmdWxsbmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBtYWlsLmNvbSIsInBhc3N3b3JkIjoiYWRtaW4xMjMiLCJpZHN0YXR1cyI6MSwiaWRyb2xlIjoxLCJvdHAiOiJheFliYyIsImlhdCI6MTYzMDQ2MDI3MiwiZXhwIjoxNjMwNTAzNDcyfQ.bq3fS3eQUM2xsygGQNlMiOAB7kZawM_QRcBzknH3jfA"
+}
+```
+
+## Financial Report
+
+### Read Income
+Request:
+- Method: GET
+- Endpoint: `/budget/income`
+Response:
+```json
+{
+    "total": 317000,
+    "month": 128000,
+    "day": 128000,
+    "data": [
+        {
+            "date": "2021-08-09T15:15:49.000Z",
+            "income": 20000
+        },
+        {
+            "date": "2021-08-10T15:25:32.000Z",
+            "income": 30000
+        },
+        {
+            "date": "2021-08-12T08:12:45.000Z",
+            "income": 10000
+        },
+        {
+            "date": "2021-08-20T08:12:45.000Z",
+            "income": 13000
+        },
+        {
+            "date": "2021-08-21T08:08:36.000Z",
+            "income": 60000
+        },
+        {
+            "date": "2021-08-22T08:12:36.000Z",
+            "income": 22000
+        },
+        {
+            "date": "2021-08-24T08:12:36.000Z",
+            "income": 34000
+        },
+        {
+            "date": "2021-09-01T00:30:34.000Z",
+            "income": 128000
+        }
+    ]
+}
+```
+
+### Read Expense
+Request:
+- Method: GET
+- Endpoint: `/budget/expenses`
+Response:
+```json
+{
+    "expenses": 190000,
+    "month": 60000,
+    "day": 60000,
+    "data": [
+        {
+            "date": "2021-08-12T08:12:45.000Z",
+            "expenses": 90000
+        },
+        {
+            "date": "2021-08-14T08:12:45.000Z",
+            "expenses": 40000
+        },
+        {
+            "date": "2021-09-01T00:30:34.000Z",
+            "expenses": 60000
+        }
+    ]
+}
+```
--- controllers/FinancialReportController.js
+++ controllers/FinancialReportController.js
@@ -4,23 +4,16 @@ module.exports = {
     GetIncome: async (req, res, next) => {
         try {
             if (req.user.idrole === 1) {
-                let queryIncome = `Select DATE(date_transaction) AS date, SUM(subtotal_harga_jual - subtotal_harga_beli) as income
-                FROM tb_transaction GROUP BY DATE(date_transaction) ORDER BY date;`
-                let queryTopProductIncome = `Select td.idproduct, p.nama, MAX(kuantity) as amount, (subtotal_harga_jual - subtotal_harga_beli) as profit from tb_transaction_detail td join tb_product p on td.idproduct = p.idproduct
-                join tb_transaction t on td.idtransaction=t.idtransaction;`
-                let queryModal = `Select SUM(subtotal_harga_beli) as sum_beli FROM tb_transaction;`
-                let queryUserSpent = `Select SUM(subtotal_harga_jual) as user_spent FROM tb_transaction;`
+                let queryIncome = `Select date_transaction AS date, SUM(subtotal_harga_jual - subtotal_harga_beli) as income
+                FROM tb_transaction GROUP BY DATE(date) ORDER BY date;`
                 let queryTotalIncome = `Select SUM(subtotal_harga_jual - subtotal_harga_beli) as total_income FROM tb_transaction;`
                 let queryCurrentMonthIncome = `SELECT SUM(subtotal_harga_jual - subtotal_harga_beli) as income FROM tb_transaction WHERE MONTH(date_transaction)=MONTH(curdate());`
                 let queryCurrentDateIncome = `SELECT SUM(subtotal_harga_jual - subtotal_harga_beli) as income FROM tb_transaction WHERE DATE(date_transaction)=curdate();`
                 let total = await dbQuery(queryTotalIncome)
-                let modal = await dbQuery(queryModal)
-                let userSpent = await dbQuery(queryUserSpent)
                 let month = await dbQuery(queryCurrentMonthIncome)
                 let day = await dbQuery(queryCurrentDateIncome)
                 let data = await dbQuery(queryIncome)
-                let topProduct = await dbQuery(queryTopProductIncome)
-                res.status(200).send({ total: total[0].total_income, modal: modal[0].sum_beli, userSpent: userSpent[0].user_spent, month: month[0].income, day: day[0].income, data: data, top: topProduct })
+                res.status(200).send({ total: total[0].total_income, month: month[0].income, day: day[0].income, data: data })
             } else {
                 res.status(400).send({ message: "Must be admin" })
             }
@@ -33,8 +26,15 @@ module.exports = {
         try {
             if (req.user.idrole === 1) {
                 let queryExpenses = `Select Sum(total) as expenses from tb_restock_product;`
+                let queryDataExpense = `Select date AS date, total as expenses
+                FROM tb_restock_product GROUP BY DATE(date) ORDER BY date;`
+                let queryCurrentMonthExpense = `SELECT SUM(total) as expenses FROM tb_restock_product WHERE MONTH(date)=MONTH(curdate());`
+                let queryCurrentDateExpense = `SELECT SUM(total) as expenses FROM tb_restock_product WHERE DATE(date)=curdate();`
+                let dataExpense = await dbQuery(queryDataExpense)
                 let expenses = await dbQuery(queryExpenses)
-                res.status(200).send({ expenses: expenses[0].expenses })
+                let month = await dbQuery(queryCurrentMonthExpense)
+                let day = await dbQuery(queryCurrentDateExpense)
+                res.status(200).send({ expenses: expenses[0].expenses, month: month[0].expenses, day: day[0].expenses, data: dataExpense })
             } else {
                 res.status(400).send({ message: "Must be admin" })
             }
